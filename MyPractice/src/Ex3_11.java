79d81722e2af85cd2cabd279907501f855307ddd
hyunjuuun <hyunjun86120@gmail.com>
2020-07-05 17:54:52 +0900

    형 변환을 이용하여 원하는 값 출력 추가

--- MyPractice/src/Ex3_11.java
+++ MyPractice/src/Ex3_11.java
@@ -6,6 +6,12 @@ public class Ex3_11 {
 		double pi = 3.141592;
 		double shortPi = Math.round(pi * 1000)/1000.0;
 		System.out.println(shortPi);
+		
+		// 3.141을 얻고 싶다면?
+		
+		System.out.println(pi*1000);
+		System.out.println((int)(pi*1000));
+		System.out.println((int)(pi*1000)/1000.0);
 	}
 
 }

2a99f58c18b0870925467789baea1327527cc8b6
hyunjuuun <hyunjun86120@gmail.com>
2020-07-10 15:17:30 +0900

    변수 i의 값을 1로 초기화

--- Review/src/MaxMinInArray.java
+++ Review/src/MaxMinInArray.java
@@ -8,7 +8,7 @@ public class MaxMinInArray {
 		int max = score[0];
 		int min = score[0];
 
-		for (int i = 0; i < score.length; i++) {
+		for (int i = 1; i < score.length; i++) {
 			if (score[i] > max) {
 				max = score[i];
 			} else if (score[i] < min) {

af02361cf96873c81f7fa5be342e7e27fc54ed7e
hyunjuuun <hyunjun86120@gmail.com>
2020-07-10 15:16:14 +0900

    배열에서 최대값과 최소값 출력하기 연습

---
+++ Review/src/MaxMinInArray.java
@@ -0,0 +1,22 @@
+
+public class MaxMinInArray {
+
+	public static void main(String[] args) {
+
+		int[] score = { 78, 95, 100, 354, 20, 120, 50 };
+
+		int max = score[0];
+		int min = score[0];
+
+		for (int i = 0; i < score.length; i++) {
+			if (score[i] > max) {
+				max = score[i];
+			} else if (score[i] < min) {
+				min = score[i];
+			}
+		}
+		System.out.println("최대값: " + max);
+		System.out.println("최소값: " + min);
+	}
+
+}
